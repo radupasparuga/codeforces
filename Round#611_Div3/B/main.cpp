7ea07af6d90949983e0a5cd1a10a1386ae08e631
radupasparuga <radupasparuga25@gmail.com>
2019-12-28 20:23:51 +0200

    Fuck b

---
+++ Round#611_Div3/B/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() { 
+  ios_base::sync_with_stdio(false); 
+  cin.tie(NULL);
+  int t;
+  cin >> t;
+  for(int i = 0; i < t; ++i) {
+    int n, k, L, R;
+    cin >> n >> k;
+    if(n%k == 0){
+      cout << n << endl;
+    }else {
+      L = n/k;
+      R = (n/k)+1;
+      int b=0, a=0, b_sum = 0;
+      while(b < k/2) {
+        b_sum += R;
+        b++;
+      }
+      if((n-b_sum)%L == 0){ 
+        if((n-b_sum)/L == k-b){
+          cout << n << endl;
+        } else {
+          cout << b_sum + k-b << endl;
+        }
+      } else {
+          cout << n-(n-b_sum)%L << endl;   
+      }
+    }
+  }
+  return 0;
+}
