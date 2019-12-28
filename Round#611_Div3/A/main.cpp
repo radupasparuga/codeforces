4146df9dc32c9ea87b75ce8776faf327100a54aa
radupasparuga <radupasparuga25@gmail.com>
2019-12-28 19:11:35 +0200

    A

--- Round#611_Div3/A/main.cpp
+++ Round#611_Div3/A/main.cpp
@@ -8,7 +8,14 @@ int main() {
   int t;
   cin >> t;
   for(int i = 0; i < t; ++i) {
-    
+    int h,m, hours_in_minutes;
+    cin >> h >> m;
+    hours_in_minutes = 24-h;
+    if(hours_in_minutes <= 1) hours_in_minutes = 0;
+    else {
+      hours_in_minutes = (24-h-1)*60;
+    }
+    cout << 60-m+hours_in_minutes << endl;
   }
   return 0;
 }
