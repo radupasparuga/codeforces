e592caf0f969b0cfa429ef841e4a51563ca6665d
radupasparuga <radupasparuga25@gmail.com>
2020-01-26 00:00:55 +0200

    Finished code i guess?

--- oji_2019/1/main.cpp
+++ oji_2019/1/main.cpp
@@ -7,13 +7,15 @@ ifstream in("conexidad.in");
 ofstream out("conexidad.out");
 int viz[102], a[102][5001], n, m, k;
 int b[102][5001];
+int c1[102], n1;//componente conexe cu 1 singur nod
+int c2[102], n2;//componente conexe cu mai multe noduri
 
 
 void DFS(int x) {
   viz[x] = 1;
-  b[k][0]++, b[k][b[k][0]] = x;
+  b[k][0] = b[k][0] + 1, b[k][b[k][0]] = x;
   for(int i = 1; i <=a[x][0]; ++i)
-    if(!viz[A[x][i]])
+    if(!viz[a[x][i]])
       DFS(a[x][i]);
 }
 
@@ -39,6 +41,55 @@ int main() {
   
   int nr = k - 1;
 
-  for(int i = 1; i <= n; ++i) viz[i] = 0;
+  for (int i = 1; i <= n; ++i) viz[i] = 0;
+  for (int i = 1; i <= k; ++i) {
+    if (b[i][0] == 1) {
+      n1++; // componenta conexa 1 nod
+      c1[n1] = i;
+    } else {
+      n2++; // componenta conexa mai multe noduri
+      c1[n2] = i;
+      viz[i] = b[i][0]; // numar noduri in componenta conexa
+    }
+  }
+
+  if (n2 == 0) { // daca avem doar noduri izolate
+    out << "2\n" << k - 1 << "\n";
+    for (int i = 1; i < n; ++i)
+      out << i << " " << i + 1 << "\n"; //legam nodurile izolate
+  } else {
+    int max_elem;
+    if (k == 1) max_elem = 0; // o singura componenta conexa
+    else {
+      if(n1 == 0) max_elem = 1;
+      else if(n1 > n - n1 - 2 - (n2 - 2) * 2) max_elem = 2;
+      else max_elem = 1;
+    }
+    out << max_elem << "\n" << k - 1 << "\n";
+    // legam componentele conexe care au mai multe noduri
+    for (int i = 1; i < n2; ++i) {
+      x = b[c2[i]][viz[c2[i]]]; 
+      y = b[c2[i + 1]][viz[c2[i + 1]]];
+      out << x << " " << y << "\n";
+      nr = nr - 1;
+      viz[c2[i]] = viz[c2[i]] - 1;
+      viz[c2[i + 1]] = viz[c2[i + 1]] - 1;
+    }
+
+    int izo = 0;
+    for (int i = 1; (i <= n2) && (nr > 0); ++i) {
+      for (int j = viz[c2[i]]; (j > 0) && (nr > 0); --j) {
+        izo++;
+        out << b[c2[i]][j] << " " << b[c1[izo]][1] << "\n";
+        nr--;
+        viz[c2[i]] = viz[c2[i]] - 1;
+      }
+    }
+    while (nr > 0) {
+      out << b[c1[izo]][1] << " " << b[c1[izo + 1]][1] << "\n";
+      nr--;
+      izo++;
+    }
+  }
   return 0;
 }
